f3b62b8dd3c2637410faf3e530b70396d81d7cce
gregjose25131 <gregjose2513@gmail.com>
2023-09-30 10:58:01 -0400

    prod

--- core/core/settings/prod.py
+++ core/core/settings/prod.py
@@ -4,14 +4,30 @@ from .base import *
 SECRET_KEY = 'django-insecure-$m+%&u8z+j)ok%kdizoa8vfd%rzxj%qu(i&5%6l2(@_m*=_*4h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'abroad',
+        'USER': 'gregori',
+        'PASSWORD': 'fender25',
+        'HOST': 'localhost',  # Puedes reemplazarlo con la dirección de tu servidor PostgreSQL si es diferente.
+        'PORT': '5432',       # El puerto predeterminado de PostgreSQL es 5432.
     }
 }
+import os
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('RAILWAY_PG_DB'),
+#         'USER': os.getenv('RAILWAY_PG_USER'),
+#         'PASSWORD': os.getenv('RAILWAY_PG_PASSWORD'),
+#         'HOST': os.getenv('RAILWAY_PG_HOST'),
+#         'PORT': os.getenv('RAILWAY_PG_PORT'),
+#     }
+# }
